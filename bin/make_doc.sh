d8cc0533e24eb7577203c01477dd66dcc2e7e17f
Martin@MBP <moogle@wwwendt.de>
2012-11-15 21:56:50 +0100

    disabled tutorials (didn't seem to work yet)

--- bin/make_doc.sh
+++ bin/make_doc.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-../../jsdoc3/jsdoc ../src ../doc/README.md --tutorials ../doc/tutorials --destination ../doc/jsdoc --verbose --template ../fancytree/bin/jsdoc3-moogle
+#../../jsdoc3/jsdoc ../src ../doc/README.md --tutorials ../doc/tutorials --destination ../doc/jsdoc --verbose --template ../fancytree/bin/jsdoc3-moogle
+../../jsdoc3/jsdoc ../src ../doc/README.md --destination ../doc/jsdoc --verbose --template ../fancytree/bin/jsdoc3-moogle
 
-cd ../../jsdoc-toolkit
-java -jar jsrun.jar app/run.js -t=templates/jsdoc --directory=../fancytree/doc/jsdoc2 --verbose ../fancytree/src/
+#cd ../../jsdoc-toolkit
+#java -jar jsrun.jar app/run.js -t=templates/jsdoc --directory=../fancytree/doc/jsdoc2 --verbose ../fancytree/src/
