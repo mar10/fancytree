b96cb0da21ccdc053879ecf4d23fff14897cc40e
Tomas <norkunas.tom@gmail.com>
2013-05-08 05:37:31 +0300

    Create jquery.fancytree.hotkeys.js

---
+++ 3rd-party/extensions/hotkeys/js/jquery.fancytree.hotkeys.js
@@ -0,0 +1,34 @@
+/**!
+ * jquery.fancytree.hotkeys.js
+ * 3rd party jQuery Hotkeys extension for jQuery Fancytree
+ *
+ * Copyright 2010, John Resig
+ * Web: https://github.com/jeresig/jquery.hotkeys/
+ *
+ * Copyright (c) 2012, Martin Wendt (http://wwWendt.de)
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://code.google.com/p/fancytree/wiki/LicenseInfe
+ */
+(function($, document) {
+  "use strict";
+
+	var initHotkeys = function(tree, data) {
+		$.each(data, function(event, keys) {
+			$.each(keys, function(key, handler) {
+				$(tree.$container).on(event, null, key, function() {
+					var node = tree.getActiveNode();
+					handler(node);
+				});
+			});
+		});
+	};
+
+	$.ui.fancytree.registerExtension("hotkeys", {
+		version: "1.0",
+                hotkeys: { },
+		treeInit: function(ctx) {
+			this._super(ctx);
+                        initHotkeys(this, ctx.options.hotkeys);
+		}
+	});
+}(jQuery, document));
