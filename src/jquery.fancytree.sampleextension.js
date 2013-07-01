60ec7f6c85c8cf4d6b8afd203e3dd8547b62fb28
Martin@MBP <github@wwwendt.de>
2013-07-01 19:16:38 +0200

    added grunt-docco2 and anotated source sample

--- src/jquery.fancytree.sampleextension.js
+++ src/jquery.fancytree.sampleextension.js
@@ -1,130 +1,132 @@
+// Extending Fanyctree
+// ===================
+
+// Every extension should have a comment header containing some information
+// about the author, copyright and licensing. Also a pointer to the latest 
+// source code.  
+// Prefix with `/*!` so the comment is not removed by the minifier.
+
 /*!
- * jquery.fancytree.sampleextension.js
+ * jquery.fancytree.childcounter.js
  * Sample extension for jquery.fancytree.js (https://github.com/mar10/fancytree/).
  *
  * Copyright (c) 2012, Martin Wendt (http://wwWendt.de)
  *
- * Dual licensed under the MIT or GPL Version 2 licenses.
+ * Licensed under the MIT license.
  *     http://code.google.com/p/fancytree/wiki/LicenseInfo
  */
 
-;(function($, window, document, undefined) {
+// To keep the global namespace clean, we wrap everything in a closure
 
+;(function($, undefined) {
+
+// Consider to use [strict mode](http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/)
 "use strict";
 
-// Allow unused variables for demonstration
+// The [coding guidelines](http://contribute.jquery.org/style-guide/js/) 
+// require jshint compliance.  
+// But for this sample, we want to allow unused variables for demonstration purpose.
+    
 /*jshint unused:false */
 
-// prevent duplicate loading
-// if ( $.ui.fancytree && $.ui.fancytree.version ) {
-//     $.ui.fancytree.warn("Fancytree: duplicate include");
-//     return;
-// }
 
-/**
- * Sample function that extends Fancytree class.
- *
- * Called like
- *     tree.fooBar("mymode");
- * for example
- *     $("#tree").fancytree("getTree").fooBar("mymode");
- *
- * @lends Fancytree.prototype
- * @requires jquery.fancytree.sampleextension.js
- */
-$.ui.fancytree._FancytreeClass.prototype.fooBar = function(mode){
-	var tree = this,
-		treeOptions = tree.options,
-		extOptions = tree.options.sampleextension,
-		color = extOptions.color;
-	//...
+// Adding methods
+// --------------
+
+// New member functions can be added to the `Fancytree` class. 
+// This function will be available for every tree instance.
+//
+//     var tree = $("#tree").fancytree("getTree");
+//     tree.countSelected(false);
+
+$.ui.fancytree._FancytreeClass.prototype.countSelected = function(topOnly){
+    var tree = this,
+        treeOptions = tree.options;
+    return tree.getSelectedNodes(topOnly).length;
 };
 
-/**
- * Sample function that extends the widget API.
- *
- * Called like
- *     $("#tree").fancytree("widgetMethod1", "abc");
- *
- * @requires jquery.fancytree.sampleextension.js
- */
+
+// The `FancytreeNode` class can also be easily extended. This would be called
+// like
+//
+//     node.toUpper();
+
+$.ui.fancytree._FancytreeNodeClass.prototype.toUpper = function(){
+    var node = this;
+    return node.setTitle(node.title.toUpperCase());
+};
+
+
+// Finally, we can extend the widget API and create functions that are called 
+// like so:
+//
+//     $("#tree").fancytree("widgetMethod1", "abc");
+
 $.ui.fancytree.prototype.widgetMethod1 = function(arg1){
-	var tree = this.tree,
-		treeOptions = tree.options,
-		extOptions = tree.options.sampleextension,
-		color = extOptions.color;
-//...
-	return arg1;
+    var tree = this.tree;
+    return arg1;
 };
 
 
-/**
- * Extension code
- */
-$.ui.fancytree.registerExtension("sampleextension", {
-	/** @type String Optional version information */
-	version: "1.0.0",
-	/** Default options for this extension. */
-	options: {
-		color: "#001122",
-		types: "active expanded focus selected"
-	},
-	/* Local function.
-	 *
-	 * Local functions are prefixed with an underscore '_'.
-	 */
-	_setKey: function(type, key, flag){
-		var cookieName = this.cookiePrefix + type,
-			cookie = $.cookie(cookieName),
-			cookieList = cookie ? cookie.split(this.delimiter) : [];
-	},
-	// Override virtual methods for this extension.
-	// `this`       : is this extension object
-	// `this._base` : the Fancytree instance
-	// `this._super`: the virtual function that was overridden (member of prev. extension or Fancytree)
-	treeInit: function(ctx){
-		var tree = ctx.tree,
-			opts = ctx.options,
-			self = this;
+// Register a Fancytree extension
+// ------------------------------
+// If we want to author a full blown extension, that is available for all trees,
+// 
+//
+//     $("#tree").fancytree({
+//         extensions: ["childcounter"],
+//         ...
+//     });
+//
 
-		// TODO: use tree ID in cookie ID prefix by default
-		this.delimiter = "";
-		this.cookiePrefix = opts.persist.cookiePrefix || "fancytree-" + tree._id + "-";
 
-		// Bind postinit-handler to apply cookie state
-		tree.$div.bind("fancytreepostinit", function(e){
-			var cookie,
-				prevFocus = $.cookie(self.cookiePrefix + "focus"), // record this before we activate
-				node;
-			tree.debug("COOKIE " + document.cookie);
-			if(self.storeFocus && prevFocus){
-				node = tree.getNodeByKey(prevFocus);
-				if(node){
-					node.setFocus();
-				}
-			}
-		});
-		// Init the tree
-		this._super(ctx);
-	},
-	/* Destroy this tree instance (we only call the default implementation, so
-	 * this method could as well be omitted). */
-	treeDestroy: function(ctx){
-		this._super(ctx);
-	},
-	nodeSetActive: function(ctx, flag) {
-		this._super(ctx, flag);
-		if(this.storeActive){
-			$.cookie(this.cookiePrefix + "active",
-					ctx.tree.activeNode ? ctx.tree.activeNode.key : null,
-					this.cookieOpts);
-		}
-	},
-	nodeSetSelected: function(ctx, flag) {
-		this._super(ctx, flag);
-		if(this.storeSelected){
-			this._setKey("selected", ctx.node.key, flag);
-		}
-	}
+/* 'childcounter' extension */
+//Every extension must be registered by a unique name.
+$.ui.fancytree.registerExtension("childcounter", {
+// Version information should be compliant with [semver](http://semver.org)
+    version: "1.0.0",
+
+// Extension specific options and their defaults.
+// This options will be available 
+
+    options: {
+        deep: true,
+        template: " <b>(%count%)</b>"
+    },
+
+// Local functions are prefixed with an underscore '_'.
+
+    _appendCounter: function(node){
+    },
+
+// Override virtual methods for this extension.
+// `this`       : is this extension object  
+// `this._base` : the Fancytree instance  
+// `this._super`: the virtual function that was overridden (member of prev. extension or Fancytree)  
+
+    treeInit: function(ctx){
+        var tree = ctx.tree,
+            opts = ctx.options,
+            self = this;
+
+        // Init the tree
+        this._super(ctx);
+    },
+
+// Destroy this tree instance (we only call the default implementation, so
+// this method could as well be omitted).
+
+    treeDestroy: function(ctx){
+        this._super(ctx);
+    },
+
+// 
+//
+
+    nodeSetSelected: function(ctx, flag) {
+        this._super(ctx, flag);
+    }
+// End of extension definition
 });
-}(jQuery, window, document));
+// End of namespace closure
+}(jQuery));
