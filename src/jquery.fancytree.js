fdfa8c3f0897ba11b14c736de817884150e4b5ae
Koloto <alkoloto@hotmail.com>
2014-03-17 21:24:31 +0400

    nodeRemoveChildren() doesn't collapse node, but resetLazy() does it

--- src/jquery.fancytree.js
+++ src/jquery.fancytree.js
@@ -1260,7 +1260,8 @@ FancytreeNode.prototype = /** @lends FancytreeNode# */{
 	 * event is triggered on next expand.
 	 */
 	resetLazy: function() {
-		this.removeChildren(); // also collapses
+		this.removeChildren();
+		this.expanded = false;
 		this.lazy = true;
 		this.children = undefined;
 		this.renderStatus();
