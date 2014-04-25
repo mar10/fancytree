5394ee36353fd0c7fc15889bce1c44037372b779
Jilong Kan <jilongk@amazon.com>
2014-04-24 18:25:09 -0700

    Enable fancytree to support multiple context menus
    Every once a while, it is necessary to support different context menus
    for different type of element, i.e. folder node is created with folder
    specific menu items, and non-folder node is appended with
    non-folder specific menu items.

--- src/jquery.fancytree.menu.js
+++ src/jquery.fancytree.menu.js
@@ -61,37 +61,39 @@ $.ui.fancytree.registerExtension({
 
 //        tree.$container[0].oncontextmenu = function() {return false;};
 		// Replace the standard browser context menu with out own
-		tree.$container.delegate("span.fancytree-node", "contextmenu", function(event) {
-			var node = $.ui.fancytree.getNode(event),
-				ctx = {node: node, tree: node.tree, originalEvent: event, options: tree.options};
-			tree.ext.menu._openMenu(ctx);
-			return false;
-		});
+        $.each(opts.menu, function(idx, menu) {
+		    tree.$container.delegate("span.fancytree-node", "contextmenu", function(event) {
+		    	var node = $.ui.fancytree.getNode(event),
+		    		ctx = {node: node, tree: node.tree, originalEvent: event, menu: menu};
+		    	tree.ext.menu._openMenu(ctx);
+		    	return false;
+		    });
 
-		// Use jquery.ui.menu
-		$(opts.menu.selector).menu({
-			create: function(event, ui){
-				tree.ext.menu.data.$menu = $(this).menu("widget");
-				var data = $.extend({}, tree.ext.menu.data);
-				opts.menu.create.call(tree, event, data);
-			},
-			focus: function(event, ui){
-				var data = $.extend({}, tree.ext.menu.data, {
-					menuItem: ui.item,
-					menuId: ui.item.find(">a").attr("href")
-				});
-				opts.menu.focus.call(tree, event, data);
-			},
-			select: function(event, ui){
-				var data = $.extend({}, tree.ext.menu.data, {
-					menuItem: ui.item,
-					menuId: ui.item.find(">a").attr("href")
-				});
-				if( opts.menu.select.call(tree, event, data) !== false){
-					tree.ext.menu._closeMenu(ctx);
-				}
-			}
-		}).hide();
+		    // Use jquery.ui.menu
+		    $(menu.selector).menu({
+		    	create: function(event, ui){
+		    		tree.ext.menu.data.$menu = $(this).menu("widget");
+		    		var data = $.extend({}, tree.ext.menu.data);
+		    		opts.menu.create.call(tree, event, data);
+		    	},
+		    	focus: function(event, ui){
+		    		var data = $.extend({}, tree.ext.menu.data, {
+		    			menuItem: ui.item,
+		    			menuId: ui.item.find(">a").attr("href")
+		    		});
+		    		opts.menu.focus.call(tree, event, data);
+		    	},
+		    	select: function(event, ui){
+		    		var data = $.extend({}, tree.ext.menu.data, {
+		    			menuItem: ui.item,
+		    			menuId: ui.item.find(">a").attr("href")
+		    		});
+		    		if( opts.menu.select.call(tree, event, data) !== false){
+		    			tree.ext.menu._closeMenu(ctx);
+		    		}
+		    	}
+		    }).hide();
+        });
 	},
 	treeDestroy: function(ctx){
 		this._super(ctx);
@@ -99,13 +101,12 @@ $.ui.fancytree.registerExtension({
 	_openMenu: function(ctx){
 		var data,
 			tree = ctx.tree,
-			opts = ctx.options,
-			$menu = $(opts.menu.selector);
+			$menu = $(ctx.menu.selector);
 
 		tree.ext.menu.data.node = ctx.node;
 		data = $.extend({}, tree.ext.menu.data);
 
-		if( opts.menu.beforeOpen.call(tree, ctx.originalEvent, data) === false){
+		if( ctx.menu.beforeOpen.call(tree, ctx.originalEvent, data) === false){
 			return;
 		}
 
@@ -119,24 +120,24 @@ $.ui.fancytree.registerExtension({
 				tree.ext.menu._closeMenu(ctx);
 			}
 		});
-//        $menu.position($.extend({my: "left top", at: "left bottom", of: event}, opts.menu.position));
+//        $menu.position($.extend({my: "left top", at: "left bottom", of: event}, ctx.menu.position));
 		$menu
 			.css("position", "absolute")
 			.show()
 			.position({my: "left top", at: "right top", of: ctx.originalEvent, collision: "fit"})
 			.focus();
 
-		opts.menu.open.call(tree, ctx.originalEvent, data);
+		ctx.menu.open.call(tree, ctx.originalEvent, data);
 	},
 	_closeMenu: function(ctx){
 		var $menu,
 			tree = ctx.tree,
-			opts = ctx.options,
+            menu = ctx.menu,
 			data = $.extend({}, tree.ext.menu.data);
-		if( opts.menu.close.call(tree, ctx.originalEvent, data) === false){
+		if( menu.close.call(tree, ctx.originalEvent, data) === false){
 			return;
 		}
-		$menu = $(opts.menu.selector);
+		$menu = $(menu.selector);
 		$(document).unbind("keydown.fancytree, mousedown.fancytree");
 		$menu.hide();
 		tree.ext.menu.data.node = null;
