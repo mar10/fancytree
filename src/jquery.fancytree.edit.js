5c29dbd25df69ba0175a88ecb189af90e892a690
Martin@MBP <github@wwwendt.de>
2016-11-06 15:04:03 +0100

    Fix exception in ext-filter (regression of #658 )

--- src/jquery.fancytree.edit.js
+++ src/jquery.fancytree.edit.js
@@ -233,7 +233,7 @@ $.ui.fancytree._FancytreeNodeClass.prototype.editCreateNode = function(mode, ini
  * @requires jquery.fancytree.edit.js
  */
 $.ui.fancytree._FancytreeClass.prototype.isEditing = function(){
-	return this.ext.edit.currentNode;
+	return this.ext.edit ? this.ext.edit.currentNode : null;
 };
 
 
@@ -244,7 +244,7 @@ $.ui.fancytree._FancytreeClass.prototype.isEditing = function(){
  * @requires jquery.fancytree.edit.js
  */
 $.ui.fancytree._FancytreeNodeClass.prototype.isEditing = function(){
-	return this.tree.ext.edit.currentNode === this;
+	return this.tree.ext.edit ? this.tree.ext.edit.currentNode === this : false;
 };
 
 
