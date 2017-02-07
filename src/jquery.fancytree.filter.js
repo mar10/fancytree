ad52322092719879a875e5a5e4244560898fa576
Jasper Krijgsman <jd.krijgsman@gmail.com>
2017-02-07 19:31:36 +0100

    Fix/Performance: All 'span.fancytree-title' elements in the document are selected when node.span is undefined. (#601)
    This fixes a high CPU problem when using the filter extension with many tree nodes.

--- src/jquery.fancytree.filter.js
+++ src/jquery.fancytree.filter.js
@@ -213,7 +213,7 @@ $.ui.fancytree._FancytreeClass.prototype.clearFilter = function(){
 	}
 	this.visit(function(node){
 		if( node.match ) {  // #491
-			$title = $(">span.fancytree-title", node.span);
+			$title = $(node.span).find(">span.fancytree-title");
 			if( escapeTitles ) {
 				$title.text(node.title);
 			} else {
@@ -308,7 +308,7 @@ $.ui.fancytree.registerExtension({
 			node = ctx.node,
 			tree = ctx.tree,
 			opts = ctx.options.filter,
-			$title = $("span.fancytree-title", node.span),
+			$title = $(node.span).find("span.fancytree-title"),
 			$span = $(node[tree.statusClassPropName]),
 			enhanceTitle = ctx.options.enhanceTitle,
 			escapeTitles = ctx.options.escapeTitles;
