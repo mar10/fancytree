5ee46ae55a0d498aa56fcf90f3ad26b81745fbf7
Martin Wendt <github@wwwendt.de>
2017-09-09 18:33:42 +0200

    Relax dnd warning

--- src/jquery.fancytree.dnd5.js
+++ src/jquery.fancytree.dnd5.js
@@ -162,7 +162,7 @@ function handleDragOver(event, data) {
 		$targetTitle = $target.find("span.fancytree-title");
 
 	if(DRAG_ENTER_RESPONSE === false){
-		tree.warn("Ignore dragover, since dragenter returned false");  //, event, data);
+		tree.info("Ignore dragover, since dragenter returned false");  //, event, data);
 		// $.error("assert failed: dragenter returned false");
 		return false;
 	} else if(typeof DRAG_ENTER_RESPONSE === "string") {
