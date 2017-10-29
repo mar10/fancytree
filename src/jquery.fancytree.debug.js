8c36c5dcadef24df393304419c894b6eff4eeb1a
Martin Wendt <github@wwwendt.de>
2017-10-29 13:24:17 +0100

    Fix ext-debug

--- src/jquery.fancytree.debug.js
+++ src/jquery.fancytree.debug.js
@@ -19,7 +19,7 @@
 		define( [ "jquery", "./jquery.fancytree" ], factory );
 	} else if ( typeof module === "object" && module.exports ) {
 		// Node/CommonJS
-		require("jquery.fancytree.ui-deps");
+		require("jquery.fancytree");
 		module.exports = factory(require("jquery"));
 	} else {
 		// Browser globals
@@ -34,6 +34,7 @@
  * Private functions and variables
  */
 var i,
+	FT = $.ui.fancytree,
 	HOOK_NAMES = "nodeClick nodeCollapseSiblings".split(" "),
 	EVENT_NAMES = "activate beforeActivate".split(" "),
 	HOOK_NAME_MAP = {},
@@ -46,7 +47,7 @@ for(i=0; i<EVENT_NAMES.length; i++){ EVENT_NAME_MAP[EVENT_NAMES[i]] = true; }
  * Extension code
  */
 $.ui.fancytree.registerExtension({
-	name: "tracecalls",
+	name: "debug",
 	version: "@VERSION",
 	// Default options for this extension.
 	options: {
@@ -55,10 +56,15 @@ $.ui.fancytree.registerExtension({
 		traceHooks: false,  // `true`or list of event names
 		timings: false  // `true`or list of event names
 	},
+	_logHook: function(name, ctx, extra) {
+		var obj = ctx.node || ctx.tree;
+		obj.info("hook."+name, extra, ctx);
+	},
 	// Overide virtual methods for this extension.
 	// `this`       : is this Fancytree object
 	// `this._super`: the virtual function that was overridden (member of prev. extension or Fancytree)
-	treeInit: function(ctx){
+	treeInit: function(ctx) {
+		this._local._logHook("treeInit", ctx);
 		var tree = ctx.tree;
 
 		// Bind init-handler to apply cookie state
@@ -66,65 +72,66 @@ $.ui.fancytree.registerExtension({
 			tree.debug("COOKIE " + document.cookie);
 		});
 		// Init the tree
-		this._superApply(arguments);
+		return this._superApply(arguments);
 	},
 	nodeClick: function(ctx) {
-		if(this.options.tracecalls.traceHooks){
-			this.debug();
-		}
+		this._local._logHook("nodeClick", ctx, FT.eventToString(ctx.originalEvent));
+		return this._superApply(arguments);
 	},
-	nodeCollapseSiblings: function(ctx) {
-	},
-	nodeDblclick: function(ctx) {
-	},
+	// nodeCollapseSiblings: function(ctx) {
+	// },
+	// nodeDblclick: function(ctx) {
+	// },
 	nodeKeydown: function(ctx) {
+		this._local._logHook("nodeKeydown", ctx, FT.eventToString(ctx.originalEvent));
+		return this._superApply(arguments);
 	},
-	nodeLoadChildren: function(ctx, source) {
-	},
-	nodeOnFocusInOut: function(ctx) {
-	},
-	nodeRemoveChildMarkup: function(ctx) {
-	},
-	nodeRemoveMarkup: function(ctx) {
-	},
+	// nodeLoadChildren: function(ctx, source) {
+	// },
+	// nodeRemoveChildMarkup: function(ctx) {
+	// },
+	// nodeRemoveMarkup: function(ctx) {
+	// },
 	nodeRender: function(ctx, force, deep, collapsed, _recursive) {
 		// ctx.tree.debug("**** PROFILER nodeRender");
-		var s = this.options.prefix + "render '" + ctx.node + "'";
+		var res,
+			s = "render '" + ctx.node + "'";
 		/*jshint expr:true */
 		window.console && window.console.time && window.console.time(s);
-		this._superApply(arguments);
+		res = this._superApply(arguments);
 		window.console && window.console.timeEnd && window.console.timeEnd(s);
+		return res;
 	},
-	nodeRenderStatus: function(ctx) {
-	},
-	nodeRenderTitle: function(ctx, title) {
-	},
-	nodeSetActive: function(ctx, flag, callOpts) {
-	},
-	nodeSetExpanded: function(ctx, flag, callOpts) {
-	},
-	nodeSetFocus: function(ctx) {
-	},
-	nodeSetSelected: function(ctx, flag, callOpts) {
-	},
-	nodeSetStatus: function(ctx, status, message, details) {
-	},
-	nodeToggleExpanded: function(ctx) {
-	},
-	nodeToggleSelected: function(ctx) {
-	},
-	treeClear: function(ctx) {
-	},
-	treeCreate: function(ctx) {
-	},
-	treeDestroy: function(ctx) {
-	},
-//	treeInit: function(ctx) {
-//	},
-	treeLoad: function(ctx, source) {
-	},
-	treeSetFocus: function(ctx, flag) {
-	}
+// 	nodeRenderStatus: function(ctx) {
+// 	},
+// 	nodeRenderTitle: function(ctx, title) {
+// 	},
+// 	nodeSetActive: function(ctx, flag, callOpts) {
+// 	},
+// 	nodeSetExpanded: function(ctx, flag, callOpts) {
+// 	},
+// 	nodeSetFocus: function(ctx) {
+// 	},
+// 	nodeSetSelected: function(ctx, flag, callOpts) {
+// 	},
+// 	nodeSetStatus: function(ctx, status, message, details) {
+// 	},
+// 	nodeToggleExpanded: function(ctx) {
+// 	},
+// 	nodeToggleSelected: function(ctx) {
+// 	},
+// 	treeClear: function(ctx) {
+// 	},
+// 	treeCreate: function(ctx) {
+// 	},
+// 	treeDestroy: function(ctx) {
+// 	},
+// //	treeInit: function(ctx) {
+// //	},
+// 	treeLoad: function(ctx, source) {
+// 	},
+// 	treeSetFocus: function(ctx, flag) {
+// 	}
 });
 
 // Value returned by `require('jquery.fancytree..')`
