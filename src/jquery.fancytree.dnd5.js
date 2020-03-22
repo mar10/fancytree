112faa3b6fb584d8c85ef43b269e96fe86d1719e
Martin Wendt <github@wwwendt.de>
2020-03-22 11:47:55 +0100

    Cleanup

--- src/jquery.fancytree.dnd5.js
+++ src/jquery.fancytree.dnd5.js
@@ -807,6 +807,7 @@
 				r = dndOpts.dragEnter(node, data);
 
 				res = normalizeDragEnterResponse(r);
+				// alert("res:" + JSON.stringify(res))
 				DRAG_ENTER_RESPONSE = res;
 
 				allowDrop = res && (res.over || res.before || res.after);
