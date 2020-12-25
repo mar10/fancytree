10f288073c34f8e93ac43614d8572e32ff59d39c
Sankarshan <sankarshanaj@gmail.com>
2020-12-25 23:34:25 +0530

    add unit tests for the fuzzy match highlight fix

--- test/unit/test-ext-filter.js
+++ test/unit/test-ext-filter.js
@@ -28,6 +28,20 @@ function countMatched(root) {
 	return count;
 }
 
+function countHighlightedChars(filter, tree) {
+	var nodesCount = tree.filterNodes(filter);
+	var nodes = [];
+	tree.getRootNode().visit(function (n) {
+		if (n.isMatched()) {
+			nodes.push(n);
+		}
+	});
+	const count = nodes.reduce(function (count, n) {
+		 return count += $(n.span).find('.fancytree-title mark').length
+	}, 0);
+	return count;
+}
+
 /*******************************************************************************
  * test data
  */
@@ -164,6 +178,141 @@ QUnit.test("Default options, mode='hide'", function(assert) {
 			//
 			assert.equal(tree.filterNodes("Sub-item 1.1", {leavesOnly: true}),
 				  2, "'Sub-item 1.1' / leavesOnly matched 2 nodes");
+
+			done();
+		}
+	});
+});
+
+QUnit.test("Default options, fuzzy=true", function(assert) {
+	// Creating a separate source so that other test cases won't get affected.
+	var SOURCE = [
+		{title: "simple node (no explicit id, so a default key is generated)" },
+		{key: "4", title: "this nodes title has [.?*+^$[\\]\\\\(){}|-]"},
+		{key: "6", title: "node with some extra classes (will be added to the generated markup)" },
+		{key: "10", title: "Folder 1", folder: true, children: [
+			{key: "10_1", title: "Sub-item 1.1", children: [
+				{key: "10_1_1", title: "Sub-item 1.1.1"},
+				{key: "10_1_2", title: "Sub-item 1.1.2"}
+			]},
+			{key: "10_2", title: "Sub-item 1.2", children: [
+				{key: "10_2_1", title: "Sub-item 1.2.1"},
+				{key: "10_2_2", title: "Sub-item 1.2.2"}
+			]}
+		]},
+		{key: "20", title: "Simple node with active children (expand)", expanded: true, children: [
+			{key: "20_1", title: "Sub-item 2.1", children: [
+				{key: "20_1_1", title: "Sub-item 2.1.1"},
+				{key: "20_1_2", title: "Sub-item 2.1.2"}
+			]},
+			{key: "20_2", title: "Sub-item 2.2", children: [
+				{key: "20_2_1", title: "Sub-item 2.2.1"},
+				{key: "20_2_2", title: "Sub-item 2.2.2"}
+			]}
+		]},
+		{key: "30", title: "Lazy folder", folder: true, lazy: true }
+	];
+	var done = assert.async();
+
+	assert.expect(12);
+	tools.setup(assert);
+
+	$("#tree").fancytree({
+		extensions: ["filter"],
+		filter: {
+			fuzzy: true,
+			autoExpand: true,
+		},
+		source: SOURCE,
+		generateIds: true, // for testing
+		init: function(event, data){
+			var tree = data.tree;
+
+			assert.equal(tree.filterNodes("("), 4, "'(' Highlighted 1 char");
+			assert.equal(tree.filterNodes(")"), 4, "')' Highlighted 1 char");
+			assert.equal(tree.filterNodes("[.?*+^$[\\]\\\\(){}|-]"), 1, "'[.?*+^$[\\]\\\\(){}|-]' Fuzzy Matched 1 node");
+			assert.equal(tree.filterNodes("Sub-m2"), 10, "'Sub-m2' Fuzzy matched 10 nodes");
+			assert.equal(tree.filterNodes("Sub-m2"), 10, "'Sub-m2' Fuzzy matched 10 nodes");
+			assert.equal(tree.filterNodes("Sub-item 1."), 8, "'Sub-m2' Fuzzy matched 8 nodes");
+			// test how many chars are highlighted
+			assert.equal(countHighlightedChars("(", tree), 4, "'(' Fuzzy Matched 4 chars");
+			assert.equal(countHighlightedChars(")", tree), 4, "')' Fuzzy Matched 4 chars");
+			assert.equal(countHighlightedChars("[.?*+^$[\\]\\\\(){}|-]", tree), "[.?*+^$[\\]\\\\(){}|-]".length, "'[.?*+^$[\\]\\\\(){}|-]' Fuzzy Matched " + "[.?*+^$[\\]\\\\(){}|-]".length + " chars");
+			assert.equal(countHighlightedChars("Sub-m2", tree), 10 * "Sub-m2".length, "'Sub-m2' Fuzzy matched " + 10 * "Sub-m2".length + " chars");
+			assert.equal(countHighlightedChars("Sub-m2", tree), 10 * "Sub-m2".length, "'Sub-m2' Fuzzy matched " + 10 * "Sub-m2".length + " chars");
+			assert.equal(countHighlightedChars("Sub-item 1.", tree), 8 * "Sub-item 1.".length, "'Sub-m2' Fuzzy matched " + 6 * "Sub-item 1.".length + " nodes");
+
+			done();
+		}
+	});
+});
+
+QUnit.test("Default Options, escapeTitle=true, fuzzy=true", function(assert) {
+	// Creating a separate source so that other test cases won't get affected.
+	var SOURCE = [
+		{title: "simple node (no explicit id, so a default key is generated)" },
+		{title: "<script>Escaped One?</script>" },
+		{title: "<script>Escaped Two?</script>" },
+		{title: "<script>Escaped Three?</script>" },
+		{key: "4", title: "this nodes title has [.?*+^$[\\]\\\\(){}|-]"},
+		{key: "6", title: "node with some extra classes (will be added to the generated markup)" },
+		{key: "10", title: "Folder 1", folder: true, children: [
+			{key: "10_1", title: "Sub-item 1.1", children: [
+				{key: "10_1_1", title: "Sub-item 1.1.1"},
+				{key: "10_1_2", title: "Sub-item 1.1.2"}
+			]},
+			{key: "10_2", title: "Sub-item 1.2", children: [
+				{key: "10_2_1", title: "Sub-item 1.2.1"},
+				{key: "10_2_2", title: "Sub-item 1.2.2"}
+			]}
+		]},
+		{key: "20", title: "Simple node with active children (expand)", expanded: true, children: [
+			{key: "20_1", title: "Sub-item 2.1", children: [
+				{key: "20_1_1", title: "Sub-item 2.1.1"},
+				{key: "20_1_2", title: "Sub-item 2.1.2"}
+			]},
+			{key: "20_2", title: "Sub-item 2.2", children: [
+				{key: "20_2_1", title: "Sub-item 2.2.1"},
+				{key: "20_2_2", title: "Sub-item 2.2.2"}
+			]}
+		]},
+		{key: "30", title: "Lazy folder", folder: true, lazy: true }
+	];
+	var done = assert.async();
+
+	assert.expect(16);
+	tools.setup(assert);
+
+	$("#tree").fancytree({
+		extensions: ["filter"],
+		escapeTitles: true,
+		filter: {
+			fuzzy: true,
+			autoExpand: true,
+		},
+		source: SOURCE,
+		generateIds: true, // for testing
+		init: function(event, data){
+			var tree = data.tree;
+
+			assert.equal(tree.filterNodes("("), 4, "'(' Highlighted 1 char");
+			assert.equal(tree.filterNodes(")"), 4, "')' Highlighted 1 char");
+			assert.equal(tree.filterNodes("[.?*+^$[\\]\\\\(){}|-]"), 1, "'[.?*+^$[\\]\\\\(){}|-]' Fuzzy Matched 1 node");
+			assert.equal(tree.filterNodes("Sub-m2"), 10, "'Sub-m2' Fuzzy matched 10 nodes");
+			assert.equal(tree.filterNodes("Sub-m2"), 10, "'Sub-m2' Fuzzy matched 10 nodes");
+			assert.equal(tree.filterNodes("Sub-item 1."), 8, "'Sub-m2' Fuzzy matched 8 nodes");
+			assert.equal(tree.filterNodes("Esed"), 5, "'Esed' Fuzzy matched 5 nodes");
+			assert.equal(tree.filterNodes(">Esed"), 3, "'Esed' Fuzzy matched 3 nodes");
+
+			// test how many chars are highlighted
+			assert.equal(countHighlightedChars("(", tree), 4, "'(' Fuzzy Matched 4 chars");
+			assert.equal(countHighlightedChars(")", tree), 4, "')' Fuzzy Matched 4 chars");
+			assert.equal(countHighlightedChars("[.?*+^$[\\]\\\\(){}|-]", tree), "[.?*+^$[\\]\\\\(){}|-]".length, "'[.?*+^$[\\]\\\\(){}|-]' Fuzzy Matched " + "[.?*+^$[\\]\\\\(){}|-]".length + " chars");
+			assert.equal(countHighlightedChars("Sub-m2", tree), 10 * "Sub-m2".length, "'Sub-m2' Fuzzy matched " + 10 * "Sub-m2".length + " chars");
+			assert.equal(countHighlightedChars("Sub-m2", tree), 10 * "Sub-m2".length, "'Sub-m2' Fuzzy matched " + 10 * "Sub-m2".length + " chars");
+			assert.equal(countHighlightedChars("Sub-item 1.", tree), 8 * "Sub-item 1.".length, "'Sub-item 1.' Fuzzy matched " + 8 * "Sub-item 1.".length + " chars");
+			assert.equal(countHighlightedChars("Esed", tree), 5 * "Esed".length, "'Esed' Fuzzy matched " + 5 * "Esed".length + " chars");
+			assert.equal(countHighlightedChars(">Esed", tree), 3 * ">Esed".length, "'Esed' Fuzzy matched " + 3 * ">Esed".length + " chars");
 
 			done();
 		}
