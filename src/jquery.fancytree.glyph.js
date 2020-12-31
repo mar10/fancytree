f0109bf3054042afc2b0c1c83f52c35cab77dee9
mar10 <github@wwwendt.de>
2020-12-31 17:29:20 +0100

    2021

--- src/jquery.fancytree.glyph.js
+++ src/jquery.fancytree.glyph.js
@@ -4,7 +4,7 @@
  * Use glyph-fonts, ligature-fonts, or SVG icons instead of icon sprites.
  * (Extension module for jquery.fancytree.js: https://github.com/mar10/fancytree/)
  *
- * Copyright (c) 2008-2020, Martin Wendt (https://wwWendt.de)
+ * Copyright (c) 2008-2021, Martin Wendt (https://wwWendt.de)
  *
  * Released under the MIT license
  * https://github.com/mar10/fancytree/wiki/LicenseInfo
--- src/jquery.fancytree.multi.js
+++ src/jquery.fancytree.multi.js
@@ -4,7 +4,7 @@
  * Allow multiple selection of nodes  by mouse or keyboard.
  * (Extension module for jquery.fancytree.js: https://github.com/mar10/fancytree/)
  *
- * Copyright (c) 2008-2020, Martin Wendt (https://wwWendt.de)
+ * Copyright (c) 2008-2021, Martin Wendt (https://wwWendt.de)
  *
  * Released under the MIT license
  * https://github.com/mar10/fancytree/wiki/LicenseInfo
